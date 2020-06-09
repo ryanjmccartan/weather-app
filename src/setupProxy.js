4dbc7b76e0f75d1298923de8ca5b5da66edab066
Ryan McCartan <ryanjmccartan@gmail.com>
2020-06-08 20:11:09 -0500

    fixed proxy error that was preventing client side from running

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,6 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(
-    proxy(["/api", , "/otherApi"], { target: "http://localhost:5000" })
+  app.use("/api", createProxyMiddleware({ target: "http://localhost:5000", changeOrigin: true })
   );
 };
